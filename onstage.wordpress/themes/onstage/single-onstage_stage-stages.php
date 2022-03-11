0c4e7bc65ced295bd72a61288da29f476ad9561f
Kyra Hendriks <kyra.hendriks@alliander.com>
2022-03-11 15:50:42 +0100

    Nu dus met de detailpagina erbij.....

--- onstage.wordpress/themes/onstage/single-onstage_stage-stages.php
+++ onstage.wordpress/themes/onstage/single-onstage_stage-stages.php
@@ -2,8 +2,85 @@
 
 ?>
 
-<h2>Data gedreven de energietransitie te lijf</h2>
-<p>Het team System Management bouwt aan IT oplossingen om Liander steeds beter te laten omgaan met de vele veranderingen door de Energietransitie. Als &eacute;&eacute;n van de initiatieven bouwt dit team aan applicaties om meetdate en events om te zetten naar slimme en geautomatiseerde bedrijfsprocessen.
-</p>
-<h3>Wat ga jij doen?</h3>
-<p>Binnen deze opdracht zul jij dus voor </p>
+<div class="row">
+    <div class="col-8">
+        <h1>De energie Data broker</h1>
+    </div>
+    <div class="col-4 text-right">
+        <button class="btn btn-primary">Reageer! <i class="fas fa-angle-right"></i></button>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <p class="lead">Ben jij onze nieuwe Energie Data Broker? Bij deze opdracht ga je aan de slag binnen bij het team
+            Databrokers. Onze ambitie is om ervoor te zorgen dat iedereen (binnen en buiten Alliander) beschikt over
+            actuele, correcte en complete assetdata en nettopologie volgens een heldere standaard, zodat we onze doelen
+            op het gebied van maakbaarheid en efficiency kunnen realiseren.</p>
+    </div>
+    <div class="col-12">
+        <h2>Wat ga JIJ doen?</h2>
+    </div>
+    <div class="col-12">
+        <p>Wij zoeken een gedreven afstudeerder die zich wil verdiepen in het vak van DevOps engineer / Software
+            Developer en daarbij kennis wil opdoen van Openshift en Kubernetes. Het Databrokers team faciliteert heel
+            Alliander in een variate aan actuele dataproducten. Bij deze opdracht zul je simultaan werken aan de
+            doorontwikkeling van het Alliander API framework en zult de kennis hiervan en de toepassing overbrengen naar
+            de teams die deze data gaan gebruiken.</p>
+    </div>
+    <div class="col-12">
+        <p>Daarnaast kom je terecht in een warme en collegiale omgeving, waar vragen stellen gewaardeerd wordt, op elk
+            vlak van de techniek waar je maar interesse in hebt. Natuurlijk zorgen we ervoor dat je goede begeleiding
+            krijgt. Je kunt ook altijd bij mentor en collega's terecht om lastige vraagstukken samen op te lossen.</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h3>Onderwerpen</h3>
+                <ul>
+                    <li>Energietransitie</li>
+                    <li>Data management</li>
+                    <li>Software development</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card bg-green">
+            <div class="card-body">
+                <i class="fas fa-icon-clock"></i>
+                <div>6 maanden</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-4">
+        <div class="card">
+            <div class="card-body">
+                <h3>Competenties</h3>
+                <ul>
+                    <li>Software ontwikkeling</li>
+                    <li>Data Engineering</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-6">
+        <div class="card">
+            <div class="card-body">
+                <h3>Organisatie</h3>
+                <p>Deze stage vindt plaats bij Alliander binnen het team Databrokers.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card bg-lightgreen">
+            <div class="card-body">
+                <div>€ 500,-</div>
+                <div>per maand + laptop in bruikleen + reiskosten</div>
+            </div>
+        </div>
+    </div>
+</div>
