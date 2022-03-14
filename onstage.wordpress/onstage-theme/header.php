adea6825b6f9ef37fa9fe73af7d5a932063a14ac
Kyra Hendriks <kyra.hendriks@alliander.com>
2022-03-14 15:35:12 +0100

    Fixed issue #5 hoofdnavigatie

--- onstage.wordpress/onstage-theme/header.php
+++ onstage.wordpress/onstage-theme/header.php
@@ -12,28 +12,51 @@
         <link rel="icon" type="image/png" href="plaatjes/on-stage-logo-favicon2.png"/>
     </head>
     <body>
-        <header>
-            <?php wp_nav_menu(
-                array(
-                    'menu' => 'primary',
-                    'container' => '',
-                    'theme_location' => 'primary',
-                    'items_wrap' => '<ul id="" class=""'
-                )
-            )
-            ?>
-            <nav>
-                <ul>
-                    <li class="logo"><a href="/"><img src="<?= get_theme_file_uri() ?>/assets/plaatjes/on-stage-logo.png" alt="On Stage" width="100px"></a></li>
-                    <li class="menu">
-                        <label for="menu"><i class="fas fa-hamburger"></i> Menu</label>
-                        <input type="checkbox" id="menu">
+    <header>
+        <nav>
+            <div class="container">
+                <ul class="d-flex align-items-center justify-content-between mx-3">
+                    <li class="logo">
+                        <a href="/"><img src="<?= get_theme_file_uri() ?>/assets/plaatjes/on-stage-logo.png" alt="On Stage"
+                                width="100px"></a>
                     </li>
-                    <li class="selected"><a href="index.html">Marktplaats</a></li>
-                    <li><a href="bedrijven.html">Bedrijven</a></li>
-                    <li><a href="ervaringen.html">Ervaringen</a></li>
-                    <li><a href="">Aanmelden</a></li>
-                    <li title="No login yet... :)"><a href="https://github.com/bartvanderwal/on-stage/tree/main/docs"><i class="fas fa-user"></i> Docs</a></li>
+                    <li class="d-md-none nav-item dropdown nav-mobile">
+                        <a class="nav-link dropdown-toggle btn btn-default" data-bs-toggle="dropdown" href="#" role="button"
+                            aria-expanded="false"><i class="fas fa-bars me-3"></i>Menu</a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="active dropdown-item" href="index.html">Marktplaats</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="bedrijven.html">Bedrijven</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="ervaringen.html">Ervaringen</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="">Aanmelden</a>
+                            </li>
+                            <li title="No login yet... :)">
+                                <a  class="dropdown-item" href="https://github.com/bartvanderwal/on-stage/tree/main/docs"><i class="fas fa-user"></i> Docs</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="d-none d-md-block active">
+                        <a href="index.html">Marktplaats</a>
+                    </li>
+                    <li class="d-none d-md-block">
+                        <a href="bedrijven.html">Bedrijven</a>
+                    </li>
+                    <li class="d-none d-md-block">
+                        <a href="ervaringen.html">Ervaringen</a>
+                    </li>
+                    <li class="d-none d-md-block">
+                        <a href="">Aanmelden</a>
+                    </li>
+                    <li class="d-none d-md-block" title="No login yet... :)">
+                        <a href="https://github.com/bartvanderwal/on-stage/tree/main/docs"><i class="fas fa-user"></i> Docs</a>
+                    </li>
                 </ul>
-            </nav>
-        </header>
+        </div>
+        </nav>
+    </header>
